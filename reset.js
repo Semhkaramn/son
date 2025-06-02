2cd3f726ba97e9c41b673e8fd5e7e612a1e2c48f
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2025-06-03 01:25:18 +0300

    Create reset.js

---
+++ reset.js
@@ -0,0 +1,13 @@
+var gtagscript = document.createElement('script');
+
+gtagscript.setAttribute('src','https://www.googletagmanager.com/gtag/js?id=G-WVQ6LFJY1T');
+
+document.head.appendChild(gtagscript);
+
+    
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-WVQ6LFJY1T');
+
